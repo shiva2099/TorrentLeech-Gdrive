75bdd7af71ba6102f506f2c6b791b5870178da54
shiva2099 <76653639+shiva2099@users.noreply.github.com>
2021-02-08 18:42:46 +0530

    Add files via upload

---
+++ tobrot/config.py
@@ -0,0 +1,24 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+    TG_BOT_TOKEN= "1115654mineAPZNx@gautamugvhUw"
+    APP_ID = 1399
+    API_HASH = "7e4d0fbmine10c62a41807"
+    AUTH_CHANNEL = [-100138715, -1001653, -100136861, -10014208, -1001277602, -100113303]
+    INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev"
+    GLEECH_COMMAND = "gleech@gautam"
+    YTDL_COMMAND = 'ytdl@gautam'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@gautam"
+    CLONE_COMMAND_G = "gclone@gautam"
+    PYTDL_COMMAND_G = "pytdl@gautam"
+    STATUS_COMMAND = "status@gautam"
+    DESTINATION_FOLDER = "TorrentLeech-Gdrive"
+    LEECH_COMMAND = "leech@gautam"
+    #fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
+    # Do not delete [DRIVE] #do not delete [DRIVE] but replace remaining part with yours data..if more data use common sense
+    RCLONE_CONFIG = """
+[DRIVE]
+type = drive
+scope = drive
+token = {"ac—fill—yours—data—jNc3MpTRy2PuGD_Lvsodct---fill--yours---0-18T12:11:26.58411451Z"}
+team_drive = 0ABS@gautamk9PVA"""
