61ff844d4cc2f3160a8a68003949af93bef438e9
shiva2099 <76653639+shiva2099@users.noreply.github.com>
2021-02-08 19:21:25 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -2,8 +2,8 @@ from tobrot.sample_config import Config
 
 class Config(Config):
     TG_BOT_TOKEN= "1578450031:AAEi-BgP28oOS5P4LxhLSu0TlXJ8CcoZSaA"
-    APP_ID = "2699106"
-    OWNER_ID = "1218670347"
+    APP_ID = 2699106
+    OWNER_ID = 1218670347
     API_HASH = "dbc0763f2ec0505c88130f3ab7b64484"
     AUTH_CHANNEL = [-1001153183002]
     INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev"
