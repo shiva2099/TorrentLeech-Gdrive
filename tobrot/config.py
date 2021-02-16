deee0603f83c04240bd4babd69f7e3bc7b105133
shiva2099 <76653639+shiva2099@users.noreply.github.com>
2021-02-16 20:58:22 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,20 +1,20 @@
 from tobrot.sample_config import Config
 
 class Config(Config):
-    TG_BOT_TOKEN= "1578450031:AAEi-BgP28oOS5P4LxhLSu0TlXJ8CcoZSaA"
+    TG_BOT_TOKEN= "1684944572:AAHK_HnIN1eXma7ZGtv-_Tzg73SMGbzCeLE"
     APP_ID = 2699106
     OWNER_ID = 1218670347
     API_HASH = "dbc0763f2ec0505c88130f3ab7b64484"
-    AUTH_CHANNEL = [-1001153183002]
+    AUTH_CHANNEL = [-582339729]
     INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev"
-    GLEECH_COMMAND = "gleech@Torrentsgspfirbot"
-    YTDL_COMMAND = 'ytdl@Torrentsgspfirbot'
-    TELEGRAM_LEECH_COMMAND_G = "tleech@Torrentsgspfirbot"
-    CLONE_COMMAND_G = "gclone@Torrentsgspfirbot"
-    PYTDL_COMMAND_G = "pytdl@Torrentsgspfirbot"
+    GLEECH_COMMAND = "gleech"
+    YTDL_COMMAND = 'ytdl'
+    TELEGRAM_LEECH_COMMAND_G = "tleech"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl"
     STATUS_COMMAND = "status "
     DESTINATION_FOLDER = "TorrentLeech-Gdrive"
-    LEECH_COMMAND = "leech@Torrentsgspfirbot"
+    LEECH_COMMAND = "leech"
     #fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
     # Do not delete [DRIVE] #do not delete [DRIVE] but replace remaining part with yours data..if more data use common sense
     RCLONE_CONFIG = """
